cea999b4f90fe34165131dc394e7f0afd358e69b
Ruthreshj <ruthreshj017@gmail.com>
2026-03-25 11:57:38 +0530

    Initial commit

---
+++ src/main/java/com/example/smartinventorymanagementbackend/SmartInventoryManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartinventorymanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInventoryManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartInventoryManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartinventorymanagementbackend/SmartInventoryManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartinventorymanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventoryManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
